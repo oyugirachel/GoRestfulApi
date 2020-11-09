e1ec2a87c8a3a0c12c5bb3c6045f6ed9db976dbd
oyugirachel <oyugirachel20@gmail.com>
2020-11-09 20:36:31 +0300

    go api

--- main.go
+++ main.go
@@ -1,10 +1,26 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+type Article struct {
+    Title string `json:"Title"`
+    Desc string `json:"desc"`
+    Content string `json:"content"`
+}
+
+// let's declare a global Articles array
+// that we can then populate in our main function
+// to simulate a database
+var Articles []Article
+func returnAllArticles(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(w).Encode(Articles)
+}
+
 
 func homePage(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "Welcome to the HomePage!")
@@ -13,9 +29,14 @@ func homePage(w http.ResponseWriter, r *http.Request){
 
 func handleRequests() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", returnAllArticles)
 	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
 func main() {
+	Articles = []Article{
+		Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		Article{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+	}
 	handleRequests()
 }
